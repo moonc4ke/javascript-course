781486a7d36a4726c0bb5026a5e01ee03b1cff7e
Gerda Palepšytė <gerda.palepsyte@telesoftas.com>
2022-11-28 20:12:21 +0200

    Add new info to lesson 1

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('veiki!');
+
+let firstName = 'Donatas';
+
+console.log('Hello ', firstName);
+
+firstName = 'Gerda';
+
+console.log('Hello ', firstName);
+
+let age = 23; // number
+let student = true;
+
+document.getElementById('p1').innerHTML = 'Hello ' + firstName;
+document.getElementById('p2').innerHTML = 'I am ' + age;
+document.getElementById('p3').innerHTML = 'Enrolled: ' + student;
