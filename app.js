ad06ec95ee2efbcbe79eca8d0055cb170756e18a
Donatas <adom.donatas@gmail.com>
2022-11-28 20:12:15 +0200

    Add new info to lesson 1

---
+++ app.js
@@ -0,0 +1,12 @@
+let firstName = 'Donatas';
+
+console.log('Hello ', firstName);
+
+firstName = 'Gerda';
+
+console.log('Hello ', firstName);
+
+let age = 23; // number
+let student = true; 
+
+document.getElementById('p1').innerHTML = 'Hello ' + firstName;
