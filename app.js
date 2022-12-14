781af4db8dc6b6cad4a7e5dd4b1939ccb494effc
semicolon <adom.donatas@gmail.com>
2022-12-14 20:09:56 +0200

    Final changes

--- app.js
+++ app.js
@@ -52,3 +52,30 @@ document.getElementById('myButton').onclick = function () {
   console.log(username);
   document.getElementById('myLabel').innerHTML = 'Hello ' + username;
 };
+
+
+
+// type conversion = change the datatype of a value to another
+//                                 (strings, numbers, booleans)
+
+let age = window.prompt("How old are you?");
+
+console.log(typeof age);
+age = Number(age);
+age += 1;
+
+console.log("Happy Birthday! You are", age, "years old");
+
+/*
+let x;
+let y;
+let z;
+
+x = Number("3.14");
+y = String(3.14);
+z = Boolean("pizza");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+*/
