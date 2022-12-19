a424caf26611179f1691bb9bc7d753e9fb1c87c9
semicolon <adom.donatas@gmail.com>
2022-12-19 18:58:47 +0200

    Almost finished lesson 3

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
+// Math
 
+// let maximum;
+// let minimum;
+
+// let number1 = 4;
+// let number2 = 1;
+// let number3 = 7;
+
+// maximum = Math.min(number1, number2, number3);
+
+// console.log(maximum);
+
+// let userName = 'Mike Tyson';
+// let phoneNumber = '86-30-30-646';
+
+// console.log(userName.slice(5));
+
+// let myName = 'donatas';
+
+// let firstLetter = myName.charAt(0).toUpperCase().toLowerCase();
+
+// console.log(firstLetter);
+
+let age = 26;
+
+// if (age >= 60) {
+//   console.log('As esu senas');
+// } else if (age >= 30) {
+//   console.log('Esu jau pagyvenes zmogus');
+// } else {
+//   console.log('vis dar jaunas');
+// }
+
+if (age === '26') {
+  console.log('man yra 26');
+} else {
+  console.log('man nera 26');
+}
